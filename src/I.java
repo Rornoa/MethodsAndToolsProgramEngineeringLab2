4a3fd5bb9e5e76a130c8233ffc0a69ea345728e2
User <aaa>
2021-03-21 13:34:25 +0300

    r0

---
+++ src/I.java
@@ -0,0 +1,30 @@
+public class I implements G, H {
+
+    private long h = 1234;
+
+    private long g = 1234;
+
+    public byte oo() {
+        return 3;
+    }
+
+    public java.util.Random mm() {
+        return new java.util.Random();
+    }
+
+    public int ae() {
+        return 8;
+    }
+
+    public int cc() {
+        return 39;
+    }
+
+    public void bb() {
+        System.out.println(42);
+    }
+
+    public Object gg() {
+        return return getClass().getClassLoader();
+    }
+}
---
+++ src/J.java
@@ -0,0 +1,22 @@
+public class J implements H {
+
+    private double k = 100.500;
+
+    private byte e = 1;
+
+    public int[] ii() {
+        return new int[]{4, 3, 2, 1};
+    }
+
+    public int af() {
+        return -1;
+    }
+
+    public void bb() {
+        System.out.println(getClass().getName());
+    }
+
+    public Object gg() {
+        return new java.util.Random();
+    }
+}
